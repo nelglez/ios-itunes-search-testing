fe1b25b5b041650a186b6d51c42cf93d473a3ddd
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-04 08:48:16 -0500

    Add unit test

---
+++ iTunes SearchTests/SearchResultControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultControllerTests.swift
+//  iTunes SearchTests
+//
+//  Created by Nelson Gonzalez on 3/4/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class SearchResultControllerTests: XCTestCase {
+
+
+}
